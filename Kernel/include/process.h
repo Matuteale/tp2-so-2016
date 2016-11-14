e6fe28f1d140db0ff7f570a366716d9601206df4
alexismoragues <amoragues@itba.edu.ar>
2016-11-14 16:06:09 -0300

    advances

--- Kernel/include/process.h
+++ Kernel/include/process.h
@@ -2,6 +2,7 @@
 #define PROCESS_H
 #include <stdint.h>
 #include <memManager.h>
+#include <sys/types.h>
 
 typedef struct Process {
 	void * stack;
@@ -9,11 +10,17 @@ typedef struct Process {
 	uint64_t nOfPages;
 	uint64_t memStackOffset[STACK_SIZE];
 	void * entryPoint;
+	pid_t PID;
+	pid_t ParentPID;
+
 } Process;
 
 static void * toStackAddress(void * page);
 static void * fillStackFrame(void * entryPoint, void * stack);
 static void * auxMemAlloc(Process * process, uint64_t size, uint64_t pageNum);
 static void * malloc(Process * process, uint64_t size);
+static pid_t getPID();
+static pid_t getParentPID();
+
 
 #endif
