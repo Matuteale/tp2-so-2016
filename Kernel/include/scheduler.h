c076b47f249371e65c33af13f6c762c4e2e33ef4
matuteale <mbuscagl@itba.edu.ar>
2016-12-03 12:26:21 -0300

    Fix error 2

--- Kernel/include/scheduler.h
+++ Kernel/include/scheduler.h
@@ -16,7 +16,7 @@ pid_t addProcess(void * entryPoint, char * name);
 //void removeProcess(Process * process);
 int removeProcess(pid_t pid);
 void * mem_alloc();
-void * userSchedToKernel(void * rsp);
+void * userSchedToKernel(uint64_t * rsp);
 void * kernelSchedToUser();
 
 #endif
