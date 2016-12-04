c3fa1ec138f62cc8c2c9bbcb1976e18f2f76a452
matuteale <mbuscagl@itba.edu.ar>
2016-12-04 17:19:18 -0300

    m

--- Userland/CodeModule/shell.c
+++ Userland/CodeModule/shell.c
@@ -95,6 +95,7 @@ int parse_command(char * s1, char * s2)
 /* funcion de ayuda */
 void help()
 {
+	while(1){
 	printString("--------------------< shell commands >--------------------\n");
 	printString("systime - mostrar la hora y fecha del sistema \n");
 	printString("setsystime - modificar la hora y fecha del sistema\n");
@@ -105,6 +106,8 @@ void help()
 	printString("beep - reproduce un sonido durante un tiempo determinado\n");
 	printString("music - reproduce una cancion (estrellita)\n");
 	printString("----------------------------------------------------------\n");
+}
+
 
 	return;
 }
