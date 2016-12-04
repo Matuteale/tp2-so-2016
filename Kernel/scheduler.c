1e0fda5ab39f4652d60c42c0ab8546f34af52ab7
matuteale <mbuscagl@itba.edu.ar>
2016-12-04 15:22:30 -0300

    m

--- Kernel/scheduler.c
+++ Kernel/scheduler.c
@@ -226,19 +226,21 @@ pid_t addProcess(void * entry_point, char * name) {
 
 	--freeProcesses;
 	ncNewline();
-
-	ncPrint("EntryPoint: ");
-	ncPrintHex(new_process->entryPoint);
-	ncNewline();
-	ncPrint("stack: ");
-	ncPrintHex(new_process->stack);
-	ncNewline();
-	// ncPrint("next: ");
-	// ncPrintDec(new_process->next->PID);
-	ncNewline();
 	ncPrint("PID: ");
 	ncPrintDec(new_process->PID);
-	ncNewline();
+
+	// ncPrint("EntryPoint: ");
+	// ncPrintHex(new_process->entryPoint);
+	// ncNewline();
+	// ncPrint("stack: ");
+	// ncPrintHex(new_process->stack);
+	// ncNewline();
+	// ncPrint("next: ");
+	// ncPrintDec(new_process->next->PID);
+	// ncNewline();
+	// ncPrint("PID: ");
+	// ncPrintDec(new_process->PID);
+	// ncNewline();
 
 	return new_process->PID;
 
