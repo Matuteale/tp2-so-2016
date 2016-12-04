2106b7183d7a0e27dc74f6939d4f6985920cd2fd
matuteale <mbuscagl@itba.edu.ar>
2016-12-04 13:48:16 -0300

    m

--- Userland/CodeModule/shell.c
+++ Userland/CodeModule/shell.c
@@ -62,7 +62,7 @@ void shell()
 /*Lista de procesos*/
 void ps()
 {
-	int processPID[16];
+	int processPID[16] = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
 	printString("PID\n");
 	ps_sys(processPID);
 	int i = 0;
