2ee67ca065d2096d829a06785f8659ef31a8cc06
matuteale <mbuscagl@itba.edu.ar>
2016-12-04 23:33:46 -0300

    m

--- Kernel/scheduler.c
+++ Kernel/scheduler.c
@@ -257,10 +257,8 @@ int removeProcess(pid_t pid) {
 		currentProcess->state = INACTIVE;
 		if(currentProcess->next->PID == 1){
 			currentProcess->next->next->state = ACTIVE;
-			currentProcess = currentProcess->next->next;
 		}else{
 			currentProcess->next->state = ACTIVE;
-			currentProcess = currentProcess->next;
 		}
 		scheduleNow();
 	}
