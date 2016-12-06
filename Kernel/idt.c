36d771028d69d502f4baf91dca1c3298a17d7058
matuteale <mbuscagl@itba.edu.ar>
2016-12-06 18:28:17 -0300

    a

--- Kernel/idt.c
+++ Kernel/idt.c
@@ -109,7 +109,7 @@ void create_process(void * entryPoint, char * name, int isBackground)
   }
   Process * process = getCurrentProcess();
   Process * processAux = process;
-  while(process->next->PID != procesAux->PID){
+  while(process->next->PID != processAux->PID){
     ncPrint(process->name);
     process = process->next;
   }
