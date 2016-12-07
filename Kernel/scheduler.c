186b5e7e413ab19af3d3efe0c7625ce3251fa862
matuteale <mbuscagl@itba.edu.ar>
2016-12-06 21:23:16 -0300

    a

--- Kernel/scheduler.c
+++ Kernel/scheduler.c
@@ -117,7 +117,7 @@ void setNextProcess(){
 		currentProcess = current;
 		currentProcess->state = RUNNING;
 		if(currentProcess->PID == shellProcess->PID){
-			pit_setup(600);
+			pit_setup(10000);
 		}else{
 			pit_setup(100);
 		}
