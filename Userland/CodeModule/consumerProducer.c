92eab5818ccff35a2e56668917990a44645960e8
matuteale <mbuscagl@itba.edu.ar>
2016-12-08 17:11:52 -0300

    a

--- Userland/CodeModule/consumerProducer.c
+++ Userland/CodeModule/consumerProducer.c
@@ -50,7 +50,7 @@ int mutexp;
 
 void mainProdCons() {
 	openMessageQ("pcMQ", &pcMQ);
-	printString(pcMQ.name);
+	printDec(pcMQ[1]);
 	sys_addProcess("producer", producer, 1);
 	sys_addProcess("consumer", consumer, 1);
 	while(1){
