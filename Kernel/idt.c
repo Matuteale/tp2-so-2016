b6ac3e925210052cb5e7195ba005852643f23ff5
matuteale <mbuscagl@itba.edu.ar>
2016-12-09 11:25:52 -0300

    a

--- Kernel/idt.c
+++ Kernel/idt.c
@@ -211,7 +211,7 @@ void sleepProcess(long milis){
 void listIPCs(char ** ipcs, int * values)
 {
   int i = getOpenedMessageQs(ipcs);
-  ncPrint(ipcs[0]);
+  ncPrint(&(ipcs[0]));
   ipcs[i] = "Mutex";
   values[i] = 1;
   i++;
