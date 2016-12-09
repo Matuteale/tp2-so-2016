9c49032a53360b9fbb043f1654ad94cc33be1856
matuteale <mbuscagl@itba.edu.ar>
2016-12-09 13:50:11 -0300

    a

--- Userland/CodeModule/shell.c
+++ Userland/CodeModule/shell.c
@@ -108,7 +108,7 @@ void ipcs()
 		ipcs[0] = names[i*21];
 		i++;
 	}
-	char * aux = &(*(ipcs[0]));
+	char * aux = ipcs[0];
 	printString(aux);
 	printString("IPC | Value \n");
 	ipcs_sys(ipcs, names, values);
