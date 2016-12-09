e26e642d3f632e2679a2eecfb5a2d6be0bca0511
matuteale <mbuscagl@itba.edu.ar>
2016-12-09 10:42:11 -0300

    a

--- Kernel/msgqueues.c
+++ Kernel/msgqueues.c
@@ -21,6 +21,7 @@ int getOpenedMessageQs(char ** openedQueues){
       int size = 0;
       while(msgQNames[i][size++] != 0);
       memcpy(openedQueues[j], msgQNames[i], size + 1);
+      ncPrint(openedQueues[j])
       j++;
     }
   }
