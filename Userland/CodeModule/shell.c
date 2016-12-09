8a4e5060bc90693971b62c8d173003578969d666
matuteale <mbuscagl@itba.edu.ar>
2016-12-09 10:55:26 -0300

    a

--- Userland/CodeModule/shell.c
+++ Userland/CodeModule/shell.c
@@ -103,13 +103,13 @@ void ipcs()
 	printString("IPC | Value \n");
 	ipcs_sys(ipcs, values);
 	int i = 0;
-	while(ipcs[i][0] != 0){
-		printString(ipcs[i]);
-		printString(" - ");
-		printDec(values[i]);
-		printString("\n");
-		i++;
-	}
+	// while(ipcs[i][0] != 0){
+	// 	printString(ipcs[i]);
+	// 	printString(" - ");
+	// 	printDec(values[i]);
+	// 	printString("\n");
+	// 	i++;
+	// }
 	int activePID;
 	sys_getActivePID(&activePID);
 	sys_killProcess(activePID);
