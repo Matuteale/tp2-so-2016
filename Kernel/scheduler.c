d7d3f2f12949df868801f1bb4e95693c9590ccb3
matuteale <mbuscagl@itba.edu.ar>
2016-12-10 19:31:42 -0300

    a

--- Kernel/scheduler.c
+++ Kernel/scheduler.c
@@ -320,12 +320,12 @@ pid_t addProcess(void * entry_point, char * name, int isBackground) {
 }
 
 void freeProcess(pid_t pid) {
-	ncPrint("El current es: ");
 	ncPrint("saco el: ");
 	ncPrintDec(pid);
 	ncPrint("El current es: ");
 	ncPrint(currentProcess->name);
 	ncPrintDec(currentProcess->state);
+	cPrintDec(currentProcess->PID);
 	Process * prevProcess = currentProcess;
 	Process * processToRemove;
 	while(prevProcess->next->PID != pid) {
