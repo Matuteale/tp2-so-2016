55507eb6fcd20885a4df22f9fdd38eade8b89954
matuteale <mbuscagl@itba.edu.ar>
2016-12-11 19:39:27 -0300

    a

--- Kernel/scheduler.c
+++ Kernel/scheduler.c
@@ -149,7 +149,7 @@ void setNextProcess(){
 
 void wakeOrContinueSleep(){
 	for(int i = 0; i < 16; i++){
-		if(waitingProcess[i] != 666){
+		if(waitingProcess[i] == 666){
 			timertickFlags[i]--;
 			ncPrintDec(timertickFlags[i]);
 			ncPrint("-");
