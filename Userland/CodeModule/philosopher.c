ccd284e4daf14c378c7a295d39c6aa16a83c7ea1
matuteale <mbuscagl@itba.edu.ar>
2016-12-11 22:27:48 -0300

    a

--- Userland/CodeModule/philosopher.c
+++ Userland/CodeModule/philosopher.c
@@ -66,9 +66,9 @@ void philosopher() {
 	int aux = 0;
 	while(1) {
 		if(philosopherCount > 1 && !addingPhil){
-			sys_sleep(500);
+			sys_sleep(200);
 			takeForks(id);
-			sys_sleep(100*(id+1));
+			sys_sleep(400*(id+1));
 			putForks(id);
 		}
 	}
