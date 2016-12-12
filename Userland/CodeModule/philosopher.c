bc291d9ee16b2de516f6cc4c27467e82e0b4774a
matuteale <mbuscagl@itba.edu.ar>
2016-12-12 16:42:27 -0300

    add mutex.asm

--- Userland/CodeModule/philosopher.c
+++ Userland/CodeModule/philosopher.c
@@ -67,9 +67,9 @@ void philosopher() {
 	int aux = 0;
 	while(1) {
 		if(philosopherCount > 1 && !addingPhil){
-			sys_sleep(200);
+			sys_sleep(2000);
 			takeForks(id);
-			sys_sleep(4000*(id+1));
+			sys_sleep(400*(id+1));
 			putForks(id);
 		}
 	}
