d0c6edfe9223d30edff18cc148da4fa5af40d172
matuteale <mbuscagl@itba.edu.ar>
2016-12-11 22:35:05 -0300

    a

--- Userland/CodeModule/philosopher.c
+++ Userland/CodeModule/philosopher.c
@@ -66,7 +66,7 @@ void philosopher() {
 	int aux = 0;
 	while(1) {
 		if(philosopherCount > 1 && !addingPhil){
-			sys_sleep(100);
+			sys_sleep(600);
 			takeForks(id);
 			sys_sleep(200*(id+1));
 			putForks(id);
