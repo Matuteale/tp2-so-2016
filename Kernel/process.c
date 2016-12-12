bb5eb465e7e6bd6bf148c3321d3dd5bcbc4af238
matuteale <mbuscagl@itba.edu.ar>
2016-12-12 16:56:02 -0300

    fix warnings

--- Kernel/process.c
+++ Kernel/process.c
@@ -34,13 +34,13 @@ typedef struct StackFrame {
 	uint64_t ss;
 	uint64_t base;
 }StackFrame;
-	
+
 
 	void initializeProcessTable(void * entryPoint) {
 		ProcessTable * PTable;
 		PTable->counter = 0;
 		addProcess(entryPoint);
-	} 
+	}
 
 
 
@@ -56,36 +56,6 @@ typedef struct StackFrame {
 	void * toStackAddress(void * page) {
 		return (uint8_t*)page + PAGE_SIZE - 0x10;
 	}
-
-	void * fillStackFrame(void * entryPoint, void * stack) {
-		StackFrame * frame = (StackFrame*)stack - 1;
-		frame->gs =		0x001;
-		frame->fs =		0x002;
-		frame->r15 =	0x003;
-		frame->r14 =	0x004;
-		frame->r13 =	0x005;
-		frame->r12 =	0x006;
-		frame->r11 =	0x007;
-		frame->r10 =	0x008;
-		frame->r9 =		0x009;
-		frame->r8 =		0x00A;
-		frame->rsi =	0x00B;
-		frame->rdi =	0x00C;
-		frame->rbp =	0x00D;
-		frame->rdx =	0x00E;
-		frame->rcx =	0x00F;
-		frame->rbx =	0x010;
-		frame->rax =	0x011;
-		frame->rip =	(uint64_t)entryPoint;
-		frame->cs =		0x008;
-		frame->eflags = 0x202;
-		frame->rsp =	(uint64_t)&(frame->base);
-		frame->ss = 	0x000;
-		frame->base =	0x000;
-
-	return frame;
-	}
-
 
 	static pid_t getPID(Process * process) {
 		return process->PID;
